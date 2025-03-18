3b7e7df3b013d0ccf44d355a6950b6cbfb5d695c
saraeindor <saraeindor@gmail.com>
2025-03-18 14:28:16 +0200

    init project

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const port = 3000;
+
+// חיבור למונגוDB
+mongoose.connect('mongodb://localhost:27017/mydb')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Error connecting to MongoDB:', err));
+
+// הגדרת route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+// התחלת השרת
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
